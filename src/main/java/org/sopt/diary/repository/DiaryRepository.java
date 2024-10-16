af7c22cfe4004ded0201228b3d7d6c4911dd1c21
kgy1008 <gayeon1008@g.skku.edu>
2024-10-16 21:30:07 +0900

    feat: 일기 작성 5분 제약 사항 추가

--- src/main/java/org/sopt/diary/repository/DiaryRepository.java
+++ src/main/java/org/sopt/diary/repository/DiaryRepository.java
@@ -1,11 +1,12 @@
 package org.sopt.diary.repository;
 
+import java.util.List;
+import java.util.Optional;
 import org.sopt.diary.domain.Category;
 import org.sopt.diary.domain.DiaryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 public interface DiaryRepository extends JpaRepository<DiaryEntity, Long> {
@@ -13,4 +14,7 @@ public interface DiaryRepository extends JpaRepository<DiaryEntity, Long> {
     boolean existsByTitle(String title);
 
     List<DiaryEntity> findByCategory(Category category);
+
+    @Query("SELECT d FROM DiaryEntity d ORDER BY d.createdAt DESC")
+    Optional<DiaryEntity> findTopByOrderByCreatedAtDesc();
 }
--- src/main/java/org/sopt/diary/service/DiaryService.java
+++ src/main/java/org/sopt/diary/service/DiaryService.java
@@ -1,5 +1,7 @@
 package org.sopt.diary.service;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.sopt.diary.api.dto.request.DiaryCategoryRequest;
 import org.sopt.diary.api.dto.request.DiaryPostRequest;
@@ -24,6 +26,7 @@ public class DiaryService {
     }
 
     public DiaryIdResponse createDiary(final DiaryPostRequest request) {
+        validateLastDiaryTime();
         final Category category = Category.getEnumCategoryFromStringCategory(request.category());
         final DiaryEntity diary = new DiaryEntity(request.title(), request.content(), category);
         validateDiaryTitle(diary);
@@ -58,6 +61,7 @@ public class DiaryService {
         return DiaryResponse.of(diary);
     }
 
+    @Transactional(readOnly = true)
     public DiaryListResponse getCategoryDiaryList(DiaryCategoryRequest request) {
         Category category = Category.getEnumCategoryFromStringCategory(request.category());
         List<DiaryGetResponse> diaries = diaryRepository
@@ -76,6 +80,19 @@ public class DiaryService {
     private void validateDiaryTitle(final DiaryEntity diary) {
         if (diaryRepository.existsByTitle(diary.getTitle())) {
             throw new IllegalArgumentException("이미 존재하는 제목입니다. 다른 제목을 입력해주세요.");
+        }
+    }
+
+    private void validateLastDiaryTime() {
+        diaryRepository.findTopByOrderByCreatedAtDesc()
+                .ifPresent(lastDiary -> validateDiaryTime(lastDiary.getCreatedAt()));
+    }
+
+    private void validateDiaryTime(LocalDateTime lastCreatedAt) {
+        LocalDateTime now = LocalDateTime.now();
+        Duration duration = Duration.between(lastCreatedAt, now);
+        if (duration.toMinutes() < 5) {
+            throw new IllegalStateException("마지막 일기 작성 후 5분 내에는 새로운 일기를 작성할 수 없습니다.");
         }
     }
 }
